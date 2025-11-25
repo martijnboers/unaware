59e11fe75b229aac0772136bfb554b2ef76ec1b9
Martijn Boers <martijn@boers.email>
2025-11-25 15:27:26 +0100

    test

--- devenv.nix
+++ devenv.nix
@@ -11,10 +11,5 @@
   enterShell = ''
     go version
   '';
-
-  tasks."build:linux".exec = "CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/unaware-linux-amd64";
-  tasks."build:windows".exec = "CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/unaware-windows-amd64.exe";
-  tasks."build:macos".exec = "CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o dist/unaware-darwin-amd64";
-
 }
 
