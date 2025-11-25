4df25cf915bf6cee6c9e4963b8e385a903456e75
Martijn Boers <2955898+martijnboers@users.noreply.github.com>
2025-11-25 12:54:39 +0000

    Create go.yml

--- devenv.nix
+++ devenv.nix
@@ -1,16 +1,20 @@
-{ ... }:
 {
-  languages.go.enable = true;
+  pkgs,
+  ...
+}:
+{
+  # https://devenv.sh/languages/
+  languages.go = {
+    enable = true;
+  };
 
-  # For tests noexec and builds
   enterShell = ''
-    mkdir -p .tmp dist
+    go version
   '';
 
-  env.TMPDIR = ".tmp";
-  env.GOTMPDIR = ".tmp";
+  tasks."build:linux".exec = "CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/unaware-linux-amd64";
+  tasks."build:windows".exec = "CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/unaware-windows-amd64.exe";
+  tasks."build:macos".exec = "CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o dist/unaware-darwin-amd64";
 
-  scripts.build-linux.exec = "CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/unaware-linux-amd64";
-  scripts.build-windows.exec = "CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/unaware-windows-amd64.exe";
-  scripts.build-macos.exec = "CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o dist/unaware-darwin-amd64";
 }
+
